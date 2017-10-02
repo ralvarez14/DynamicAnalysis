ca5ace1a3880729fc0eec51cb693806bef094a62
Ana Garcia <ajgarcia09@Anas-MacBook-Air.local>
2017-10-02 16:10:20 -0600

    first commit

---
+++ src/Passphrase1.java
@@ -0,0 +1,17 @@
+/**
+ * Created by ajgarcia09 on 10/1/17.
+ */
+public class Passphrase1 {
+
+    public static void main(String[]args){
+        int foundFirst;
+        int foundSecond;
+        int isPasswordCorrect = checkPassword(foundFirst, foundSecond);
+    }
+
+    public static int checkPassword(int foundFirst, int foundSecond){
+        
+    }
+
+
+}
