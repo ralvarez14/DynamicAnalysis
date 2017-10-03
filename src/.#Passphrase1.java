ef4f5f8a85ad2ec8ac69d6655f5fcc925c855140
Hector Cervantes <hcervantes@miners.utep.edu>
2017-10-03 13:16:09 -0600

    Modified number1.java

---
+++ src/.#Passphrase1.java
@@ -0,0 +1,1 @@
+sasquatch@Hectors-MacBook-Pro-2.local.12381
--- src/number1.java
+++ src/number1.java
@@ -1,26 +1,40 @@
 public class number1{
     public static void main(String[] args){
-	String input = args[0];
+	System.out.println("Welcome reverser. Recently I discovered that a malicious mad scientist");
+	System.out.println("will release the T-virus into the world. Our only hope is to release");
+	System.out.println("the triggering mechanism by entering a 5 passphrase sequence. The world is in danger");
+	System.out.println("unless you (yes, you, the cs5375 student) discover the 5 passphrases before");
+	System.out.println("all of the earth is taken over by flesh eating zombies ... :( ");
+       
+        // call the first tirgger
+        trigger1(args[0]);
+}
+    
+    // executes the first trigger
+    public static void trigger1(String input){
 	boolean c = false;
-	boolean s = false;
+        boolean s = false;
         boolean exclamation = false;
-        
-	for(int i = 0; i < input.length(); i++){
-	    if(input.charAt(i) == 'c')
-		c = true;
-	    
-	    if(input.charAt(i) == 's')
-		s = true;
-	    
-	    if(input.charAt(i) == '!')
-		exclamation = true;
-	}
-	
-	if(c && s && !exclamation){
-	    System.out.println(" Trigger 1 diffused ");
-	}
-	else{
-	    System.out.println(" Wrong! ");
-	}
+        // do the iteration part
+        for(int i = 0; i < input.length(); i++){
+            if(input.charAt(i) == 'c')
+                c = true;
+
+            if(input.charAt(i) == 's')
+                s = true;
+
+            if(input.charAt(i) == '!')
+                exclamation = true;
+        }
+        // print out the tigger diffuesed if a c && s found
+        if(c && s && !exclamation){
+            System.out.println(" Trigger 1 diffused ");
+        }
+	// Print out "wrong password". Kill code
+        else{
+            System.out.println(" Wrong! ");
+	    System.exit(1);
+        }
     }
 }
+
